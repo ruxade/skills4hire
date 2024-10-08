79fbbc71426908efce7acac6de6c609a5d2b8caf
Bruna de Oliveira <bruna.soliveira@icloud.com>
2024-10-08 18:29:23 +0100

    Added Booking Controllers and View

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  before_action :check_logged_in
+
+  def index
+    @bookings = current_user.bookings
+
+  end
+
+  def new
+    @skill = Skill.find(params[:skill_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @skill = Skill.find(params[:skill_id])
+    @booking = Booking.create(booking_params)
+    @booking.skill = @skill
+    @booking.user = current_user
+    @booking.save
+
+    redirect_to bookings_path
+  end
+
+  # private
+
+  def booking_params
+    params.require(:booking).permit(:status, :skill_id)
+  end
+
+  def check_logged_in
+    if !current_user
+      redirect_to new_user_session_path
+    end
+  end
 end
